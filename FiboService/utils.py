90d343adca6039c294df343c6ce02697d4d6f0aa
Pouria <Pouria.Tohidi.G@gmail.com>
2024-02-12 04:41:51 -0500

    Rename A and B to mat_A and mat_B for better readability.

--- FiboService/utils.py
+++ FiboService/utils.py
@@ -25,10 +25,10 @@ def str_to_verbosity(s: str) -> Verbosity:
 Matrix2x2 = tuple[int, int, int, int]
 
 
-def mat_mul(A: Matrix2x2, B: Matrix2x2) -> Matrix2x2:
+def mat_mul(mat_A: Matrix2x2, mat_B: Matrix2x2) -> Matrix2x2:
     return (
-        A[0] * B[0] + A[1] * B[2],
-        A[0] * B[1] + A[1] * B[3],
-        A[2] * B[0] + A[3] * B[2],
-        A[2] * B[1] + A[3] * B[3],
+        mat_A[0] * mat_B[0] + mat_A[1] * mat_B[2],
+        mat_A[0] * mat_B[1] + mat_A[1] * mat_B[3],
+        mat_A[2] * mat_B[0] + mat_A[3] * mat_B[2],
+        mat_A[2] * mat_B[1] + mat_A[3] * mat_B[3],
     )
